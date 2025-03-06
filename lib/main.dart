a2ea7afe962382b3392a931eb6ed9fed9818170a
codenameupik <mtaufiqurrahman13@gmail.com>
2025-03-06 10:36:13 +0700

    add constant for colors

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,6 @@
-import 'package:cplz_store/utils/theme/theme.dart';
 import 'package:flutter/material.dart';
+import 'package:cplz_store/app.dart';
 
 void main() {
   runApp(const App());
-}
-
-class App extends StatelessWidget {
-  const App({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      themeMode: ThemeMode.system,
-      theme: CAppTheme.lightTheme,
-      darkTheme: CAppTheme.darkTheme,
-    );
-  }
 }
