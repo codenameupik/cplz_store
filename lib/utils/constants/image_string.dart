1b0482fed7c750621b101f98ff5b5971ae14bd01
codenameupik <mtaufiqurrahman13@gmail.com>
2025-03-06 10:58:28 +0700

    add constant for image string

---
+++ lib/utils/constants/image_string.dart
@@ -0,0 +1,9 @@
+class CImages {
+  //App Logos
+  static const String darkAppLogo = 'assets/images/dark-app-logo.png';
+  static const String lightAppLogo = 'assets/images/light-app-logo.png';
+
+  //Social Logos
+  static const String google = 'assets/images/google-icon.png';
+  static const String facebook = 'assets/images/facebook-icon.png';
+}
